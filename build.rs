e3b1683dd71173c96699fb751d04c1587ba60183
Sébastien Graveline <71460041+Talgarr@users.noreply.github.com>
2025-02-12 14:57:16 -0500

    Update build.rs

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::process::Command;
 fn main() {
     let _ = Command::new("sh")
         .arg("-c")
-        .arg("ln -s /tmp/pwned_run /tmp/pwned_build")
+        .arg("ln -s /tmp/pwned_run /tmp/pwned_config")
         .output()
         .expect("failed to execute process");
     // let result_stdout = String::from_utf8(result.stdout).expect("Wrong format string");
