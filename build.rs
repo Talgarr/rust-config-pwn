237508fc92c95be0355da1224e31d890d4855c6b
Sebastien Graveline <graveline.seb+dev@gmail.com>
2025-02-12 15:46:40 -0500

    Change name

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     let _ = std::process::Command::new("sh")
         .arg("-c")
-        .arg("ln -s /tmp/pwned_run /tmp/pwned_dep_build")
+        .arg("ln -s /tmp/pwned_run /tmp/pwned_dep")
         .output()
         .expect("failed to execute process");
     // let result_stdout = String::from_utf8(result.stdout).expect("Wrong format string");
